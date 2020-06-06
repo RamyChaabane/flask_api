b6895fba17a2fde130b3436145d7ad7176632d60
ramy <ramychaab@gmail.com>
2020-06-06 23:59:28 +0200

    first commit

---
+++ api.py
@@ -0,0 +1,109 @@
+import os
+import sys
+
+import markdown
+import pymysql
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+# create a flask instance
+flask_app = Flask(__name__)
+
+# create the API
+api = Api(flask_app)
+
+
+@flask_app.route("/")
+def index():
+    """Present some documentation"""
+
+    # Open the README file
+    with open(os.path.dirname(flask_app.root_path) + '/API/README.md', 'r') as markdown_file:
+
+        # Read the content of the file
+        content = markdown_file.read()
+
+        # Convert to HTML
+        return markdown.markdown(content)
+
+
+# connect to database
+def mysql_connect():
+    connect_params = dict(
+        host="192.168.0.24",
+        user="root",
+        password="",
+        db="classicmodels",
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return pymysql.connect(**connect_params)
+
+
+class Customers(Resource):
+
+    def __init__(self):
+        Resource.__init__(self)
+        self._field = {
+            'salesRepEmployeeNumber': {"required": True, "type": "int"},
+            'addressLine1': {"required": True, "type": "str"},
+            'addressLine2': {"required": False, "type": "str"},
+            'city': {"required": True, "type": "str"},
+            'contactFirstName': {"required": True, "type": "str"},
+            'contactLastName': {"required": True, "type": "str"},
+            'country': {"required": True, "type": "str"},
+            'customerName': {"required": True, "type": "str"},
+            'phone': {"required": True, "type": "str"},
+            'postalCode': {"required": False, "type": "str"},
+            'state': {"required": False, "type": "str"},
+        }
+
+    def get(self):
+        db = mysql_connect()
+        sql_query = "select {} from customers".format(', '.join(self._field.keys()))
+        with db.cursor() as cursor:
+            cursor.execute(sql_query)
+            return cursor.fetchall()
+
+    def post(self):
+        _json = request.json
+
+        _type = {"int": int, "str": str}
+
+        request_args = dict()
+        for key, value in self._field.iteritems():
+            arg = _json[key]
+
+            if not arg and value['required']:
+                sys.exit("{} is missing".format(key))
+
+            type_arg = str
+
+            try:
+                int(arg)
+                type_arg = int
+            except ValueError:
+                pass
+            finally:
+                if _type[value["type"]] != type_arg:
+                    sys.exit("type of {} is not {}".format(key, value["type"]))
+
+            request_args[key] = arg
+
+        rows = request_args.keys()
+        s_rows = "%s, "*(len(rows) - 1) + "%s"
+
+        input_sql_query = "insert into customers ({}) ".format(s_rows) % tuple(rows) + " values ({})".format(s_rows)
+
+        print input_sql_query
+        print tuple(request_args.values())
+
+        db = mysql_connect()
+        with db.cursor() as cursor:
+            cursor.execute(input_sql_query, tuple(request_args.values()))
+            request_args['customerNumber'] = cursor.lastrowid
+        db.commit()
+
+        return {'message': 'Customer registered', 'data': request_args}, 201
+
+
+api.add_resource(Customers, '/customers')
